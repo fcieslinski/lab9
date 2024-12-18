4165dd190c0ab5c363c9cb5a3cbb5306cd68f9e6
fakecenturyy <karpio2006@gmail.com>
2024-12-18 18:11:01 +0100

    dodanie wyszukiwania pivotu

--- src/gauss.c
+++ src/gauss.c
@@ -2,34 +2,45 @@
 #include <math.h>
 #include "gauss.h"
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
+int eliminate(Matrix *mat, Matrix *b) {
     int n = mat->r;
 
     for (int k = 0; k < n; k++) {
-        
-        if (fabs(mat->data[k][k]) < 1e-12) {
-            fprintf(stderr, "Macierz jest osobliwa. Dzielenie przez zero w wierszu %d.\n", k);
+        int max_row = k;
+        double max_val = fabs(mat->data[k][k]);
+        for (int i = k + 1; i < n; i++) {
+            if (fabs(mat->data[i][k]) > max_val) {
+                max_row = i;
+                max_val = fabs(mat->data[i][k]);
+            }
+        }
+
+        if (fabs(max_val) < 1e-12) {
+            fprintf(stderr, "Macierz jest osobliwa. Dzielenie przez zero w kolumnie %d.\n", k);
             return 1;
         }
 
-        
+        if (max_row != k) {
+            double *temp_row = mat->data[k];
+            mat->data[k] = mat->data[max_row];
+            mat->data[max_row] = temp_row;
+
+            double temp_b = b->data[k][0];
+            b->data[k][0] = b->data[max_row][0];
+            b->data[max_row][0] = temp_b;
+        }
+
         for (int i = k + 1; i < n; i++) {
             double factor = mat->data[i][k] / mat->data[k][k];
-            
-            
+
             for (int j = k; j < n; j++) {
                 mat->data[i][j] -= factor * mat->data[k][j];
             }
 
-            
             b->data[i][0] -= factor * b->data[k][0];
         }
     }
 
-		return 0;
+    return 0;
 }
 
