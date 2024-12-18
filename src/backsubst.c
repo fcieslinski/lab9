38e8da326ea5516191e6557e143fbf9402f165d4
fakecenturyy <karpio2006@gmail.com>
2024-12-18 17:48:46 +0100

    dodanie metody podstawienia wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -11,12 +11,23 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+
+    if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->c != 1 || x->r != mat->r) {
+        return 2;
+    }
+    int n = mat->r;
+    int i;
+    for (int i = n - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0.0) {
+            return 1; 
+        }
+    	double sum = b->data[i][0];
+	for (int j = i + 1; j < n; j++) {
+            sum -= mat->data[i][j] * x->data[j][0];
+        }
+	x->data[i][0] = sum / mat->data[i][i];
+    }
+    return 0;
 }
 
 
