f3818d1d7c5d748dd1c8899e4ebb89f3375bf9c3
franciszek cieslinski <franciszekcieslinski@gmail.com>
2024-12-18 17:57:21 +0100

    Dodanie potrzebnych bibliotek

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "backsubst.h"
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
@@ -5,13 +6,7 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-
     if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->c != 1 || x->r != mat->r) {
         return 2;
     }
